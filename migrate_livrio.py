5e4c0292eeead962c2252ab400536b7e5d483911
Aurélio Saraiva <aurelio@codeway.com.br>
2016-02-07 21:46:01 -0200

    migrate

--- migrate_livrio.py
+++ migrate_livrio.py
@@ -237,44 +237,8 @@ for row in result:
     db.books.update_one({'_id': get_id('book',row['id_book'])}, payload)
 
 
-print "sys_contacts_phone"
-query = """SELECT * FROM sys_contacts_phone"""
-conn.execute(query)
-result = conn.fetchall()
-for row in result:
 
-    item = decode_json(row['raw_contact'])
-    lookup = {'account_id': get_id('user',row['id_created_by']), 'contact_id': item['id']}
 
-    payload_data = {
-        '_created': to_datetime(row['registration']),
-        '_updated': to_datetime(row['updated']),
-        'source': item
-    }
-
-    emails = []
-    if item['emails']:
-        for e in item['emails']:
-            if e['value'].find('@'):
-                emails.append(e['value'])
-        payload_data['emails'] = emails
-
-    phones = []
-    if item['phoneNumbers']:
-        for e in item['phoneNumbers']:
-            if len(e['value']) >=8 :
-                phones.append(e['value'])
-        payload_data['phones'] = emails
-
-    if item['displayName']:
-        payload_data['fullname'] = item['displayName']
-
-    payload_data = remove_empty_from_dict(payload_data)
-    payload = {
-        '$set': payload_data
-    }
-
-    db.phone_contacts.update_one(lookup,payload, upsert=True)
 
 
 print "sys_history"
@@ -332,3 +296,55 @@ for row in result:
             '_created': to_datetime(row['registration'])
     })
 
+
+
+
+print "sys_contacts_phone"
+
+offset = 0
+query = "SELECT count(*) as total FROM sys_contacts_phone"
+conn.execute(query)
+result = conn.fetchone()
+
+total = result['total']
+page = int(ceil(total/5000))+1
+
+for i in range(0, page):
+    query = "SELECT * FROM sys_contacts_phone limit {},5000".format(i*5000)
+    conn.execute(query)
+    result = conn.fetchall()
+    print "PAGE: " + str(i)
+    for row in result:
+
+        item = decode_json(row['raw_contact'])
+        lookup = {'account_id': get_id('user',row['id_created_by']), 'contact_id': item['id']}
+
+        payload_data = {
+            '_created': to_datetime(row['registration']),
+            '_updated': to_datetime(row['updated']),
+            'source': item
+        }
+
+        emails = []
+        if item['emails']:
+            for e in item['emails']:
+                if e['value'].find('@'):
+                    emails.append(e['value'])
+            payload_data['emails'] = emails
+
+        phones = []
+        if item['phoneNumbers']:
+            for e in item['phoneNumbers']:
+                if len(e['value']) >=8 :
+                    phones.append(e['value'])
+            payload_data['phones'] = emails
+
+        if item['displayName']:
+            payload_data['fullname'] = item['displayName']
+
+        payload_data = remove_empty_from_dict(payload_data)
+        payload = {
+            '$set': payload_data
+        }
+
+        db.phone_contacts.update_one(lookup,payload, upsert=True)
